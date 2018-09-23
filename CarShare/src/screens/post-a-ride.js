430b2cd74391c91d8b18996a47204b73511f6ae2
Thomas1746 <thomas.fuge@gmail.com>
2018-09-23 14:03:09 +1200

    Got basic post a ride functionality working

--- CarShare/src/screens/post-a-ride.js
+++ CarShare/src/screens/post-a-ride.js
@@ -1,16 +1,52 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Button, Text} from 'react-native';
+import { FormLabel, FormInput } from 'react-native-elements';
+import firebase from 'react-native-firebase';
+import Listing from '../components/listing';
 
 export default class PostARide extends Component {
+  constructor() {
+    super();
+    this.firestoreListings = firebase.firestore().collection('listings');
+
+    this.state = {
+      listings: [],
+      meetingPoint: ''
+    }
+  }
+
+  addListing() {
+    this.firestoreListings.add({
+        timeCreated: firebase.firestore.FieldValue.serverTimestamp(),
+        meetingPoint: this.state.meetingPoint,
+        destination: 'e',
+        departureDate: '2018-08-24',
+        departureTime: '08:36',
+        seatsAvailable: 3,
+        storageSpace: true,
+        whoWantsToCome: [],
+        whosComing: []
+    });
+  }
+
   render() {
     return (
-        <View>
-            <Text>Post a ride</Text>
+        <View style = {styles.listings}>
+           <FormLabel>Meeting Place</FormLabel>
+           <FormInput name="meetingPoint" value={this.state.meetingPoint} onChangeText={text => this.setState({meetingPoint: text})}/>
+          <Button
+            title={'Submit'}
+            onPress={() => this.addListing()}
+          />
         </View>
-    );
+      );
   }
 }
 
 const styles = StyleSheet.create({
-
+  listings: {
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'center'
+  }
 });
