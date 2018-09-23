b8ab9ce37b230eed3514ed487351331770c98c3b
Thomas1746 <thomas.fuge@gmail.com>
2018-09-23 14:44:48 +1200

    Halfway through getting post a ride functionality working

--- CarShare/src/screens/PostARide/post-a-ride.js
+++ CarShare/src/screens/PostARide/post-a-ride.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Button, Text} from 'react-native';
-import { FormLabel, FormInput } from 'react-native-elements';
+import { FormLabel, FormInput, CheckBox } from 'react-native-elements';
 import firebase from 'react-native-firebase';
 import styles from './post-a-ride-styles'
 
@@ -11,7 +11,12 @@ export default class PostARide extends Component {
 
     this.state = {
       listings: [],
-      meetingPoint: ''
+      storageAvail: false,
+      noSeats: 0,
+      meetingPoint: '',
+      destination: '',
+      departureDate: '',
+      departureTime: ''
     }
   }
 
@@ -19,11 +24,11 @@ export default class PostARide extends Component {
     this.firestoreListings.add({
         timeCreated: firebase.firestore.FieldValue.serverTimestamp(),
         meetingPoint: this.state.meetingPoint,
-        destination: 'e',
-        departureDate: '2018-08-24',
-        departureTime: '08:36',
-        seatsAvailable: 3,
-        storageSpace: true,
+        destination: this.state.destination,
+        departureDate: this.state.departureDate,
+        departureTime: this.state.departureTime,
+        seatsAvailable: this.state.noSeats,
+        storageSpace: this.state.storageAvail,
         whoWantsToCome: [],
         whosComing: []
     });
@@ -32,8 +37,24 @@ export default class PostARide extends Component {
   render() {
     return (
         <View style = {styles.listings}>
-           <FormLabel>Meeting Place</FormLabel>
-           <FormInput name="meetingPoint" value={this.state.meetingPoint} onChangeText={text => this.setState({meetingPoint: text})}/>
+          <FormLabel>Space for bags?</FormLabel>
+          <CheckBox checked={this.state.storageAvail} onPress={() => this.setState({storageAvail: !this.state.storageAvail})} />
+          
+          <FormLabel>No. Seats Available:</FormLabel>
+          
+
+          <FormLabel>Meeting Place:</FormLabel>
+          <FormInput name="meetingPoint" value={this.state.meetingPoint} onChangeText={text => this.setState({meetingPoint: text})}/>
+
+          <FormLabel>Destination:</FormLabel>
+          <FormInput name="destination" value={this.state.destination} onChangeText={text => this.setState({destination: text})}/>
+
+          <FormLabel>Departure Date:</FormLabel>
+          <FormInput name="departureDate" value={this.state.departureDate} onChangeText={text => this.setState({departureDate: text})}/>
+
+          <FormLabel>Departure Time:</FormLabel>
+          <FormInput name="departureTime" value={this.state.departureTime} onChangeText={text => this.setState({departureTime: text})}/>
+
           <Button
             title={'Submit'}
             onPress={() => this.addListing()}
