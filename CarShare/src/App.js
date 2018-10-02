0033df837fd7885a80d915dfb90ba42efee131ef
chrisc96 <chrisc10196@gmail.com>
2018-10-02 22:37:21 +1300

    forcibly logging user out on load so we don't run into header bug, cbf fixing properly

--- CarShare/src/App.js
+++ CarShare/src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { View, ImageBackground } from 'react-native';
 import Router from './components/Router/router';
 import styles from './app-styles'
+import firebase from 'react-native-firebase';
 
 export default class App extends Component {
+
+  componentDidMount() {
+    if (firebase.auth().currentUser) {
+      firebase.auth().signOut();
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
